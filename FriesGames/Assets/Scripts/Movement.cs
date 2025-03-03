0415e6e2ea0d9d75164b4a38d9d94de7b1edc374
Furkan Kılıç <furkan.samet888@gmail.com>
2025-03-03 22:49:28 +0300

    I added a 2D character and edited the sprite. I also made changes to the character's face to rotate when it moves left and right.

--- FriesGames/Assets/Scripts/Movement.cs
+++ FriesGames/Assets/Scripts/Movement.cs
@@ -5,18 +5,39 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    SpriteRenderer playerRenderer;
     private float horizontalMove;
     [SerializeField] private float playerSpeed = 10f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        playerRenderer = GetComponent<SpriteRenderer>();
     }
 
     
     void Update()
     {
+        PlayerMovement();
+        FlipWithRenderer();
+    }
+
+    void PlayerMovement()
+    {
         horizontalMove = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(horizontalMove * playerSpeed, rb.velocity.y);
+    }
+
+    private void FlipWithRenderer()
+    {
+        if (horizontalMove > 0)
+        {
+            playerRenderer.flipX = false;
+        }
+
+        else if (horizontalMove < 0)
+        {
+            playerRenderer.flipX = true;
+        }
     }
 }
